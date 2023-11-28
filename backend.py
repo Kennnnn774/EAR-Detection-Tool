0e6eff4b807497cb0ffbfb188ea0786e9a36e22d
Emily Berger <bergeremily2@gmail.com>
2023-11-28 12:57:24 -0500

    worked on adding relevant db calls to backend

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from EAR_Scanner import scan_single_url  # Import your new function
 from db import search_for_url, insert_new_document
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -9,8 +10,20 @@ def scan_url():
     url = request.json.get('url')
     exists = search_for_url(url)
     if exists:
-        return jsonify(exists)
+        #if entry exists in database with this url, then check when it was last updated
+        date = datetime.strptime(exists['dateChecked'], "%m/%d/%Y, %H:%M:%S")
+        date_plus_week = date + timedelta(days=7)
+        today = datetime.now()
+
+        #Will update if last updated more than a week ago
+        if date_plus_week < today:
+            result = scan_single_url(url)
+            inserting = insert_new_document(url, result['vulnerable'])
+            return jsonify(result)
+        else:
+            return jsonify(exists)
     else:
+        #Create new entry in database because no previous entry existed
         result = scan_single_url(url)
         inserting = insert_new_document(url, result['vulnerable'])
         return jsonify(result)
