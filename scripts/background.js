cd0a5e3d38b6f0215162890c5ddba62cf2d51c99
Aya Habbas <a.habbs20@gmail.com>
2023-11-28 18:13:56 -0500

    opening page creates '...' badge

--- scripts/background.js
+++ scripts/background.js
@@ -1,18 +1,18 @@
-// chrome.tabs.onUpdated.addListener(
-//   async function(tabId, changeInfo, tab) {
-//     // read changeInfo data and do something with it
-//     // like send the new url to contentscripts.js
-//     chrome.action.setBadgeText({
-//       text: "...",
-//     });
-//     if (changeInfo.url) {
-//       chrome.tabs.sendMessage( tabId, {
-//         message: 'scan',
-//         url: changeInfo.url
-//       })
-//     }
-//   }
-// );
+chrome.tabs.onUpdated.addListener(
+  async function(tabId, changeInfo, tab) {
+    // read changeInfo data and do something with it
+    // like send the new url to contentscripts.js
+    chrome.action.setBadgeText({
+      text: "...",
+    });
+    // if (changeInfo.url) {
+    //   chrome.tabs.sendMessage( tabId, {
+    //     message: 'scan',
+    //     url: changeInfo.url
+    //   })
+    // }
+  }
+);
 
 // chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 //   if (request.vulnerable === true) {
