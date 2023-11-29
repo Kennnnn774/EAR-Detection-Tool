14eb5f097535f72399ba59b9677d3ce2cce2983b
Aya Habbas <a.habbs20@gmail.com>
2023-11-28 22:07:35 -0500

    communication between background script and content script is complete

--- scripts/background.js
+++ scripts/background.js
@@ -1,27 +1,25 @@
 chrome.tabs.onUpdated.addListener(
   async function(tabId, changeInfo, tab) {
-    // read changeInfo data and do something with it
-    // like send the new url to contentscripts.js
-    chrome.action.setBadgeText({
-      text: "...",
-    });
-    // if (changeInfo.url) {
-    //   chrome.tabs.sendMessage( tabId, {
-    //     message: 'scan',
-    //     url: changeInfo.url
-    //   })
-    // }
+    if (changeInfo.url) {
+      chrome.action.setBadgeText({
+        text: "...",
+      });
+      chrome.tabs.sendMessage(tab.id, {
+        message: "scan",
+        url: tab.url
+      });
+    }
   }
 );
 
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//   if (request.vulnerable === true) {
-//     chrome.action.setBadgeText({
-//       text: "👂",
-//     });
-//   } else {
-//     chrome.action.setBadgeText({
-//       text: "✅",
-//     });
-//   }
-// });
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.vulnerable === true) {
+    chrome.action.setBadgeText({
+      text: "👂",
+    });
+  } else {
+    chrome.action.setBadgeText({
+      text: "✅",
+    });
+  }
+});
--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,22 @@
-// chrome.runtime.onMessage.addListener(
-//   async function(request, sender, sendResponse) {
-//     // listen for messages sent from background.js
-//     if (request.message === 'scan') {
-//       // get response
-//       let result = {
-//         vulnerable: false,
-//         message: 'This site is not vulnerable!'
-//       }
-//       // if found
-//       // else conduct scan
+console.log("content script exists")
 
-//       // return result to the background script
-//       chrome.runtime.sendMessage(result);
+chrome.runtime.onMessage.addListener(
+  async function(request, sender, sendResponse) {
+    console.log("scan");
+    if (request.message === 'scan') {
+      // get response
+      let result = {
+        vulnerable: true,
+        message: 'This site is not vulnerable!'
+      }
 
-//       // store the result in local storage. so that the popup can display the result.
-//       chrome.storage.local.set(result);
-//     } 
-// });
+      // return result to background script to process result
+      chrome.runtime.sendMessage(result);
+      
+    } 
+});
+
+
 
 // document.getElementById('scanButton').addEventListener('click', function() {
 //   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
