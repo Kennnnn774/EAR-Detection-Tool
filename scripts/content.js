7d0f907145e79f4717e560380695fc992fca35cb
Aya Habbas <a.habbs20@gmail.com>
2023-12-01 03:18:00 -0500

    changed to fetch request

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,12 @@
 chrome.runtime.onMessage.addListener(
   async function(request, sender, sendResponse) {
     if (request.message === 'scan') {
-      // get response
-      let result = {
-        _id: null,
-        dateChecked: "date", 
-        message: "EAR detected",
-        status_code: 200, 
-        url: "url",
-        vulnerable: true
-      }
-
-      // return result to requester
-      chrome.runtime.sendMessage(result);
+      fetch(`${DB_URL_API_BASE}/${request.url}`)
+      .then((response) => response.json())
+      .then((data) => {
+        chrome.runtime.sendMessage(data);
+      })
+      .catch((err) => console.log(err));
     } 
 });
 
