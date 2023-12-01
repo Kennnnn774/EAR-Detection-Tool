a6fa15f2f8ec8a46896c1925bab3443ce88b6716
Aya Habbas <a.habbs20@gmail.com>
2023-12-01 16:05:19 -0500

    updated font sizes, commened out code for content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,16 @@
 chrome.runtime.onMessage.addListener(
   async function(request, sender, sendResponse) {
     if (request.message === 'scan') {
-      fetch(`${DB_URL_API_BASE}/${request.url}`)
-      .then((response) => response.json())
-      .then((data) => {
-        chrome.runtime.sendMessage(data);
-      })
-      .catch((err) => console.log(err));
+      // fetch(`${DB_URL_API_BASE}/scan`, {
+      //   body : {url : request.url}
+      // })
+      // .then((response) => response.json())
+      // .then((data) => {
+      //   if (data) {
+      //     chrome.runtime.sendMessage(data);
+      //   }
+      // })
+      // .catch((err) => console.log(err));
     } 
 });
 
