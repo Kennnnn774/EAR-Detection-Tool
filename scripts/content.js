96cd5c2aa71897ff4ecae7422e371eba8a21cca1
Aya Habbas <a.habbs20@gmail.com>
2023-12-01 17:16:06 -0500

    updated content script to access correct backend

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,12 @@
 chrome.runtime.onMessage.addListener(
   async function(request, sender, sendResponse) {
     if (request.message === 'scan') {
-      fetch(`https://ear-extension.onrender.com/scan/`, {
-        body : {
-          url : request.url
-        }
+      fetch(`https://ear-extension-backend.onrender.com/scan/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ url: request.url }),
       })
       .then((response) => response.json())
       .then((data) => {
